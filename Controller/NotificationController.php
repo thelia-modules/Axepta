b73daecb02702ff08231aa67cf3bd1fa32ca798c
nbarbey-os-alter <nbarbey@openstudio.fr>
2021-11-23 10:05:21 +0100

    fix notification controller

--- Controller/NotificationController.php
+++ Controller/NotificationController.php
@@ -5,9 +5,14 @@ namespace Axepta\Controller;
 use Axepta\Axepta;
 use Axepta\Util\Axepta as AxeptaPayment;
 use OpenApi\Controller\Front\BaseFrontOpenApiController;
+use Symfony\Component\Routing\Router;
 use Thelia\Controller\Admin\BaseAdminController;
+use Thelia\Core\Event\Order\OrderEvent;
+use Thelia\Core\Event\TheliaEvents;
 use Thelia\Model\Base\OrderQuery;
+use Thelia\Model\OrderStatusQuery;
 use Thelia\Module\BasePaymentModuleController;
+use Thelia\Tools\URL;
 
 class NotificationController extends BasePaymentModuleController
 {
@@ -23,21 +28,44 @@ class NotificationController extends BasePaymentModuleController
         $paymentResponse->setCryptKey(Axepta::getConfigValue(Axepta::CRYPT_KEY));
         $paymentResponse->setResponse($_GET);
 
-        $orderRef = $paymentResponse->getRefNr();
+        $orderRef = $paymentResponse->getrefnr();
         $order = OrderQuery::create()->filterByRef($orderRef)->findOne();
 
         $frontOfficeRouter = $this->getContainer()->get('router.front');
 
-
+        $event = new OrderEvent($order);
         if ($paymentResponse->isValid() && $paymentResponse->isSuccessful()) {
-            $TransID = $paymentResponse->getPayID();
-            $PCNr = $paymentResponse->getPCNr();
-            $CCBrand = $paymentResponse->getCCBrand();
-            $CCExpiry = $paymentResponse->getCCExpiry();
 
+            if (!$order->isPaid()) {
+                $event->setStatus(OrderStatusQuery::getPaidStatus()->getId());
+                $this->dispatch(TheliaEvents::ORDER_UPDATE_STATUS, $event);
+            }
+
+            return $this->generateRedirect(
+                URL::getInstance()->absoluteUrl(
+                    $frontOfficeRouter->generate(
+                        "order.placed",
+                        ["order_id" => $order->getId()],
+                        Router::ABSOLUTE_URL
+                    )
+                )
+            );
         }
 
-
+        $event->setStatus(OrderStatusQuery::getCancelledStatus()->getId());
+        $this->dispatch(TheliaEvents::ORDER_UPDATE_STATUS, $event);
+        return $this->generateRedirect(
+            URL::getInstance()->absoluteUrl(
+                $frontOfficeRouter->generate(
+                    "order.failed",
+                    [
+                        "order_id" => $order->getId(),
+                        "message" => $paymentResponse->getDescription()
+                    ],
+                    Router::ABSOLUTE_URL
+                )
+            )
+        );
     }
 
 }
